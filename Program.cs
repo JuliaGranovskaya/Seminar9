0bc9fd529e01c913ec19dd4c9bc83504963b01a5
Julia <granovskaya.97@mail.ru>
2022-07-23 14:43:51 +0300

    add file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// Console.WriteLine("Введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// for (int i = 0; i <= N; i++)
+// {
+//     Console.Write($"{i} ");
+// }
+string PrintNumbers(int N)
+{
+    for (int i = 0; i <= N; i++)
+    {
+        Console.Write($"{i} ");
+    }
+}
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
